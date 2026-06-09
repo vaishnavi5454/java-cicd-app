3126b12c1a1c26491f52b59861a167cae9607a88
vaishnavi <vaishnavijpawar4@gmail.com>
2026-06-09 18:40:44 +0530

    Initial CI/CD project setup

---
+++ app/src/main/java/com/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from CI/CD Pipeline! Version 1.0";
+    }
+}
